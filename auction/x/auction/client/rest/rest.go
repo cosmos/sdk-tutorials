c5797e95ec3d5145b05b906ed4d07a56e76164ae
Marko Baricevic <marbar3778@yahoo.com>
2019-10-22 12:23:02 +0200

    added query routes

--- auction/x/auction/client/rest/rest.go
+++ auction/x/auction/client/rest/rest.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// RegisterRoutes registes the auction-related REST handlers to a router
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	registerTxRoutes(cliCtx, r)
+	registerQueryRoutes(cliCtx, r)
 }
