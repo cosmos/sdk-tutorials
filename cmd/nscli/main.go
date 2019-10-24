d30061341c48becb4056dd8613c00881acd8538c
Marko Baricevic <marbar3778@yahoo.com>
2019-10-24 18:20:48 +0200

    Add tx routes to rest server
    - tx routes were not on the rest server, added them in
    
    Signed-off-by: Marko Baricevic <marbar3778@yahoo.com>

--- cmd/nscli/main.go
+++ cmd/nscli/main.go
@@ -11,6 +11,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
 	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
+	authrest "github.com/cosmos/cosmos-sdk/x/auth/client/rest"
 	bankcmd "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
 	app "github.com/cosmos/sdk-application-tutorial"
 	"github.com/spf13/cobra"
@@ -66,6 +67,7 @@ func main() {
 
 func registerRoutes(rs *lcd.RestServer) {
 	client.RegisterRoutes(rs.CliCtx, rs.Mux)
+	authrest.RegisterTxRoutes(rs.CliCtx, rs.Mux)
 	app.ModuleBasics.RegisterRESTRoutes(rs.CliCtx, rs.Mux)
 }
 
