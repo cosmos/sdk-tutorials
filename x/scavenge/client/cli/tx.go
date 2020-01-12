df2796c7d49343ce406f49995bee7c9e33bc8d5f
okwme <billy.rennekamp@gmail.com>
2020-01-12 17:33:22 +0100

    disable rest

--- x/scavenge/client/cli/tx.go
+++ x/scavenge/client/cli/tx.go
@@ -20,7 +20,7 @@ import (
 func GetTxCmd(cdc *codec.Codec) *cobra.Command {
 	scavengeTxCmd := &cobra.Command{
 		Use:                        types.ModuleName,
-		Short:                      fmt.Sprintf("%S transactions subcommands", types.ModuleName),
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
 		DisableFlagParsing:         true,
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
--- x/scavenge/client/rest/query.go
+++ x/scavenge/client/rest/query.go
@@ -7,13 +7,12 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
 	"github.com/okwme/scavenge/x/scavenge/internal/types"
 )
 
 func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router) {
-// TODO: Define your GET REST endpoints
+	// TODO: Define your GET REST endpoints
 	r.HandleFunc(
 		"/scavenge/parameters",
 		queryParamsHandlerFn(cliCtx),
