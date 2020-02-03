ca16ad20a1269e7701da86a4f9f0fc4a768b17ab
Jonathan Gimeno <jgimeno@gmail.com>
2020-02-03 19:42:38 +0100

    Revert "update code to use sdk instead of old sdkerrors"
    This reverts commit dc969b2fcdfa7dacc8eb6bd521cb931ac86a2ff9.

--- nameservice/x/nameservice/internal/types/msgs.go
+++ nameservice/x/nameservice/internal/types/msgs.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // RouterKey is the module name router key
@@ -32,10 +33,10 @@ func (msg MsgSetName) Type() string { return "set_name" }
 // ValidateBasic runs stateless checks on the message
 func (msg MsgSetName) ValidateBasic() error {
 	if msg.Owner.Empty() {
-		return sdk.ErrInvalidAddress(msg.Owner.String())
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, msg.Owner.String())
 	}
 	if len(msg.Name) == 0 || len(msg.Value) == 0 {
-		return sdk.ErrUnknownRequest("Name and/or Value cannot be empty")
+		return sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "Name and/or Value cannot be empty")
 	}
 	return nil
 }
@@ -75,13 +76,13 @@ func (msg MsgBuyName) Type() string { return "buy_name" }
 // ValidateBasic runs stateless checks on the message
 func (msg MsgBuyName) ValidateBasic() error {
 	if msg.Buyer.Empty() {
-		return sdk.ErrInvalidAddress(msg.Buyer.String())
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, msg.Buyer.String())
 	}
 	if len(msg.Name) == 0 {
-		return sdk.ErrUnknownRequest("Name cannot be empty")
+		return sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "Name cannot be empty")
 	}
 	if !msg.Bid.IsAllPositive() {
-		return sdk.ErrInsufficientCoins("Bids must be positive")
+		return sdkerrors.ErrInsufficientFunds
 	}
 	return nil
 }
@@ -119,10 +120,10 @@ func (msg MsgDeleteName) Type() string { return "delete_name" }
 // ValidateBasic runs stateless checks on the message
 func (msg MsgDeleteName) ValidateBasic() error {
 	if msg.Owner.Empty() {
-		return sdk.ErrInvalidAddress(msg.Owner.String())
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, msg.Owner.String())
 	}
 	if len(msg.Name) == 0 {
-		return sdk.ErrUnknownRequest("Name cannot be empty")
+		return sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "Name cannot be empty")
 	}
 	return nil
 }
