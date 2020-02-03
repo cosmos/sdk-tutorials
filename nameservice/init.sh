3bfdf87c96ea205b9e526364a1f399cdeae9bb05
okwme <billy.rennekamp@gmail.com>
2020-02-03 10:47:53 +0100

    tweak nameservice

--- nameservice/init.sh
+++ nameservice/init.sh
@@ -3,24 +3,23 @@
 rm -rf ~/.nsd
 rm -rf ~/.nscli
 
-nsd init test --chain-id=test
+nsd init test --chain-id=namechain
 
-echo "12345678" | nscli keys add test1
-echo "12345678" | nscli keys add test2
+nscli keys add test1
+nscli keys add test2
 
-nsd add-genesis-account $(nscli keys show test1 -a) 10000000000000000000000000stake,1000000hello
-nsd add-genesis-account $(nscli keys show test2 -a) 100000000000stake,1000hello
+nsd add-genesis-account $(nscli keys show test1 -a) 10000000000000000000000000stake,1000000nametoken
+nsd add-genesis-account $(nscli keys show test2 -a) 100000000000stake,1000nametoken
 
 nscli config output json
 nscli config indent true
 nscli config trust-node true
+nscli config chain-id namechain
 
-echo "12345678" | nsd gentx --name test1
+nsd gentx --name test1
 
 echo "Collecting genesis txs..."
 nsd collect-gentxs
 
 echo "Validating genesis file..."
 nsd validate-genesis
-
-nsd start 
