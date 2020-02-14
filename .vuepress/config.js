c4b30339d1188f195a20b7845d1948558c4bd880
Marko Baricevic <marbar3778@yahoo.com>
2020-02-14 15:29:19 +0100

    fix build

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  theme: 'cosmos',
-  title: 'Cosmos SDK Tutorials',
+  theme: "cosmos",
+  title: "Cosmos SDK Tutorials",
   themeConfig: {
     // if your docs are in a different repo from your main project:
-    docsRepo: 'cosmos/sdk-tutorials',
+    docsRepo: "cosmos/sdk-tutorials",
     autoSidebar: false,
     sidebar: [{
       title: 'Tutorials',
@@ -14,47 +14,47 @@ module.exports = {
       ]
     }],
     footer: {
-      logo: '/logo-bw.svg',
-      textLink: {text: 'cosmos.network', url: 'https://cosmos.network'},
+      logo: "/logo-bw.svg",
+      textLink: {text: "cosmos.network", url: "https://cosmos.network"},
       services: [
-        {service: 'medium', url: 'https://blog.cosmos.network/'},
-        {service: 'twitter', url: 'https://twitter.com/cosmos'},
-        {service: 'linkedin', url: 'https://www.linkedin.com/company/tendermint/'},
-        {service: 'reddit', url: 'https://reddit.com/r/cosmosnetwork'},
-        {service: 'telegram', url: 'https://t.me/cosmosproject'},
-        {service: 'youtube', url: 'https://www.youtube.com/c/CosmosProject'}
+        {service: "medium", url: "https://blog.cosmos.network/"},
+        {service: "twitter", url: "https://twitter.com/cosmos"},
+        {service: "linkedin", url: "https://www.linkedin.com/company/tendermint/"},
+        {service: "reddit", url: "https://reddit.com/r/cosmosnetwork"},
+        {service: "telegram", url: "https://t.me/cosmosproject"},
+        {service: "youtube", url: "https://www.youtube.com/c/CosmosProject"}
       ],
       smallprint:
-          'This website is maintained by Tendermint Inc. The contents and opinions of this website are those of Tendermint Inc.',
+          "This website is maintained by Tendermint Inc. The contents and opinions of this website are those of Tendermint Inc.",
       links: [
         {
-          title: 'Documentation',
+          title: "Documentation",
           children: [
-            {title: 'Cosmos SDK', url: 'https://cosmos.network/docs'},
-            {title: 'Cosmos Hub', url: 'https://hub.cosmos.network/'},
-            {title: 'Tendermint Core', url: 'https://docs.tendermint.com/'}
+            {title: "Cosmos SDK", url: "https://cosmos.network/docs"},
+            {title: "Cosmos Hub", url: "https://hub.cosmos.network/"},
+            {title: "Tendermint Core", url: "https://docs.tendermint.com/"}
           ]
         },
         {
-          title: 'Community',
+          title: "Community",
           children: [
-            {title: 'Cosmos blog', url: 'https://blog.cosmos.network/'},
-            {title: 'Forum', url: 'https://forum.cosmos.network/'},
-            {title: 'Chat', url: 'https://discord.gg/AzefAFd'}
+            {title: "Cosmos blog", url: "https://blog.cosmos.network/"},
+            {title: "Forum", url: "https://forum.cosmos.network/"},
+            {title: "Chat", url: "https://discord.gg/AzefAFd"}
           ]
         },
         {
-          title: 'Contributing',
+          title: "Contributing",
           children: [
             {
-              title: 'Contributing to the docs',
-              url: 'https://github.com/cosmos/cosmos-sdk/blob/master/docs/DOCS_README.md'
+              title: "Contributing to the docs",
+              url: "https://github.com/cosmos/cosmos-sdk/blob/master/docs/DOCS_README.md"
             },
-            {title: 'Source code on GitHub', url: 'https://github.com/cosmos/cosmos-sdk/'}
+            {title: "Source code on GitHub", url: "https://github.com/cosmos/cosmos-sdk/"}
           ]
         }
       ]
     }
   },
-  plugins: [['@vuepress/google-analytics', {ga: 'UA-51029217-13'}]],
+  plugins: [["@vuepress/google-analytics", {ga: "UA-51029217-13"}]],
 }
