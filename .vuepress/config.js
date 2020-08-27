d0c7d26f0e31997332577bbac8319411a8d4ece7
Cyrus Goh <hello@lovincyrus.com>
2020-08-27 13:30:57 -0700

    Bump docs theme to 1.0.172, Algolia DocSearch (#382)
    * bump docs theme to 1.0.171
    
    * bump, fix build compile error
    
    * [favicons] add favicon files (#385)
    
    * [favicons] add files
    
    * [favicons] change manifest name
    
    * update algolia docsearch config (#383)
    
    * Reorder readme, Custom homepage (#384)
    
    * reorder content, custom homepage
    * beginner, intermediate, advanced cards
    
    * add order to starport-blog tutorials
    
    * finalize videos section, tweak url paths in .html
    
    * footer ibc docs, video titles, minor css tweaks
    
    * remove padding-bottom of wrapper at 500px
    
    Co-authored-by: Vince MingPu Shao <vince19972@gmail.com>

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,11 +1,26 @@
 module.exports = {
   theme: "cosmos",
   title: "Cosmos SDK Tutorials",
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" }],
+    ['link', { rel: "manifest", href: "/site.webmanifest" }],
+    ['meta', { name: "msapplication-TileColor", content: "#2e3148" }],
+    ['meta', { name: "theme-color", content: "#ffffff" }],
+    ['link', { rel: "icon", type: "image/svg+xml", href: "/favicon-svg.svg" }],
+    ['link', { rel: "apple-touch-icon-precomposed", href: "/apple-touch-icon-precomposed.png" }],
+  ],  
   themeConfig: {
     repo: "cosmos/sdk-tutorials",
     docsRepo: "cosmos/sdk-tutorials",
     editLinks: true,
     label: "sdk",
+    algolia: {
+      id: "BH4D9OD16A",
+      key: "7976d773390a0be350dc24b0571eee15",
+      index: "cosmos-sdk_tutorials"
+    },
     topbar: {
       banner: false,
     },
