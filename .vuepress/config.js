e3b88b288ac9d07649c3abbff41e3158379f05d9
Tobias Schwarz <tobias@tendermint.com>
2021-03-15 11:44:57 +0100

    Use legacy tutorials extra navbar (#545)
    * Use legacy tutorials extra navbar
    
    * Add Launchpad tutorials to legacy tutorials in the navbar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -30,6 +30,22 @@ module.exports = {
         {
           title: "Tutorials",
           children: [
+
+            {
+              title: "Blog",
+              path: "/blog/tutorial/",
+              directory: true,
+            }, 
+            {
+              title: "PoFE - Migration",
+              path: "/launchpad-to-stargate/tutorial/",
+              directory: true,
+            },
+          ],
+        },
+        {
+          title: "Legacy Tutorials",
+          children: [
             {
               title: "Voter",
               path: "/voter/",
@@ -37,22 +53,12 @@ module.exports = {
             },
             {
               title: "Blog",
-              path: "/blog/tutorial/",
-              directory: true,
-            },     
-            {
-              title: "Blog (legacy)",
               path: "/blog-legacy/tutorial/",
               directory: true,
-            },            
+            },               
             {
               title: "PoFE",
               path: "/proof-of-file-existence/tutorial/",
-              directory: true,
-            },
-            {
-              title: "PoFE - Launchpad to Stargate",
-              path: "/launchpad-to-stargate/tutorial/",
               directory: true,
             },
             {
@@ -70,8 +76,8 @@ module.exports = {
               path: "/burner-chain/",
               directory: true,
             },
-          ],
-        },
+          ]
+        }
       ],
     },
     gutter: {
