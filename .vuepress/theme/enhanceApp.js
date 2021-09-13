d4cc57c455a8955052708fb0d0294f6561df6dcc
Ibo Sy <dev.coldice@gmail.com>
2021-09-13 19:09:10 +0200

    B9lab platform updates 2 - Enable preview of B9lab content (#809)
    * hide b9lab sidebar contents if not in deploy-preview
    
    * add localhost as allowed origin
    
    * fix SSR build error
    
    Co-authored-by: Gioele Bonifetto <gioele@b9lab.com>

--- .vuepress/theme/enhanceApp.js
+++ .vuepress/theme/enhanceApp.js
@@ -1,5 +1,7 @@
 import TmCodeBlock from "./global-components/TmCodeBlock.vue";
+import TmSidebarContent from "./global-components/TmSidebarContent.vue";
 
 export default ({ Vue }) => {
   Vue.component("TmCodeBlock", TmCodeBlock);
+  Vue.component("TmSidebarContent", TmSidebarContent);
 };
