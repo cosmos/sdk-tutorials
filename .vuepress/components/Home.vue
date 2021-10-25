740f28753e35e185783ea05ec8e1adbc7ce7a14c
Gioele Bonifetto <gioele@b9lab.com>
2021-10-25 17:48:09 +0200

    implements resources section

--- .vuepress/components/Home.vue
+++ .vuepress/components/Home.vue
@@ -25,7 +25,17 @@
 					.home__content__get-started__content__desc Get started right away, begin with the introductory chapter.
 			.modules
 				card-module(v-for="module in $frontmatter.modules" :module="module").modules__item
-
+			.resources__wrapper
+				h3.resources__title Developer resources
+				.resources
+					.resources__item(v-for="resource in $frontmatter.resources")
+						.resources__item__icon
+							img(:src="resource.image" :alt="resource.title")
+						h5.resources__item__title {{resource.title}}
+						.resources__item__description {{resource.description}}
+						.resources__item__links
+							a(v-for="link in resource.links" :href="link.url").resources__item__links__item {{link.name}}
+								icon-arrow.resources__item__links__item__icon
 		.home__footer
 			tm-footer(full="true")
 </template>
@@ -38,6 +48,75 @@
 
 		&__item
 			margin-top 32px
+
+	.resources
+		display flex
+		margin-top 32px
+		overflow hidden
+		overflow-x auto
+		-ms-overflow-style none
+		scrollbar-width none
+		width 100vw
+		margin-left calc(50% - 50vw)
+		padding-inline calc(50vw - 50%)
+
+		&::-webkit-scrollbar
+			display none
+
+		&__wrapper
+			margin-top 96px
+
+		&__item
+			padding 10px
+			margin-block 10px
+			display flex
+			flex-direction column
+			width 25vw
+			justify-content space-between
+
+			@media screen and (min-width: 481px) and (max-width: 1024px)
+				width 40vw
+
+			@media screen and (max-width: 480px)
+				width 80vw
+
+			&:last-child
+				padding-right 0px
+			&:first-child
+				padding-left 0px
+
+			&__icon
+				margin-right 20px
+				margin-bottom 10px
+
+				img
+					width 50px
+					height 50px
+					margin 0
+					filter var(--img-filter)
+
+			&__title
+				margin-bottom 10px
+
+			&__description
+				margin-bottom 20px
+
+			&__links
+				display flex
+
+				&__item
+					margin-block auto
+					margin-right 20px
+					cursor pointer
+					font-weight 500
+					color var(--color-text-strong)
+					display flex
+
+					&__icon
+						margin-left 5px
+						width 10px
+						height 10px
+
 
 	.mode-switch-container
 		position absolute
@@ -56,7 +135,7 @@
 
 		&__content
 			padding-inline 128px
-			margin-top 32px
+			margin-top 64px
 
 			&__get-started
 				display flex
@@ -146,6 +225,10 @@
 					
 					&__content
 						width 100%
+
+		.resources
+			&__wrapper
+				margin-top 64px
 		
 	@media screen and (min-width: 480px) and (max-width: 1024px)
 		.home
--- home/index.md
+++ home/index.md
@@ -56,4 +56,29 @@ modules:
         badge: 
           color: var(--color-secondary)
           label: Deep dive
+resources:
+  - title: Cosmos SDK
+    description: A framework for building public blockchains.
+    links:
+      - name: Documentation
+        url: 
+    image: /cosmos-sdk-icon.svg
+  - title: Tendermint Core
+    description: Blockchain consensus engine and application interface.
+    links:
+      - name: Documentation
+        url: 
+    image: /tendermint-icon.svg
+  - title: Cosmos Hub
+    description: First interconnected public blockchain on Cosmos network.
+    links:
+      - name: Documentation
+        url: 
+    image: /cosmos-sdk-icon.svg
+  - title: IBC
+    description: Industry standard protocol for inter-blockchain communication.
+    links:
+      - name: Documentation
+        url: 
+    image: /ibc-icon.svg
 ---
