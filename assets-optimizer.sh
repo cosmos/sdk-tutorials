6097ebcc072df4f90e1bf292ebb828a99da8e91e
Ibo Sy <ibo@b9lab.com>
2021-12-02 16:07:26 +0100

    fix file line endings

--- assets-optimizer.sh
+++ assets-optimizer.sh
@@ -4,7 +4,7 @@ echo "Setup: Ensure output folder is present"
 mkdir -p .vuepress/public/resized-images
 echo "Setup: Completed"
 echo "Resize: Started to resize images using sharp"
-sharp resize 800 -i {*,*/*,*/*/*,.*/*}/*.{png,jpg} -o .vuepress/public/resized-images --optimise true --withMetadata false
+sharp resize 988 -i {*,*/*,*/*/*,.*/*}/*.{png,jpg} -o .vuepress/public/resized-images --optimise true --withMetadata false
 echo "Resize: Completed"
 echo "Copy: Adds remaining small images to the same output folder"
 find . \( -name 'node_modules' -o -path './.vuepress/dist' -o -path './.vuepress/theme' -o -path './.vuepress/public/resized-images' -o -path './.vuepress/public/h5p' \) -prune -o \( -name '*.png' -o -name '*.jpg' \) -exec cp -n -v {} .vuepress/public/resized-images \;
