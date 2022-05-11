999e9ec3de256dedb88b84e5e6755cda420b37fa
Ibo Sy <ibo@b9lab.com>
2022-05-11 12:46:47 +0200

    remove 127.0.0.1 from allowedIDAOrigins

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -66,8 +66,7 @@ module.exports = {
       "deploy-preview-995", 
       "deploy-preview-994", 
       "deploy-preview-991",
-      "deploy-preview-1047",
-      "127.0.0.1"
+      "deploy-preview-1047"
     ],
     sidebar: {
       auto: false,
