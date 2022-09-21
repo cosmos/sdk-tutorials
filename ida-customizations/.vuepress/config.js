811780293503179f75e1bf21d91b3719d8e35268
Ibo Sy <ibo@b9lab.com>
2022-09-21 22:01:26 +0200

    update Makefile, ida config

--- ida-customizations/.vuepress/config.js
+++ ida-customizations/.vuepress/config.js
@@ -67,6 +67,7 @@ module.exports = {
     docsRepo: "cosmos/sdk-tutorials",
     editLinks: true,
     label: "sdk",
+    isIDAMode: true,
     algolia: {
       id: "BH4D9OD16A",
       key: "7976d773390a0be350dc24b0571eee15",
