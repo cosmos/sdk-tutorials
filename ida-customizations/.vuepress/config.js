458e1b783c9109b7274963d28235494bc60139a5
Ibo Sy <ibo@b9lab.com>
2022-11-29 00:33:13 +0100

    update config, fix docker section ref

--- ida-customizations/.vuepress/config.js
+++ ida-customizations/.vuepress/config.js
@@ -150,7 +150,7 @@ module.exports = {
                 },
                 {
                   title: "Docker Introduction",
-                  path: "/tutorials/5-docker-intro/index.html",
+                  path: "/tutorials/5-docker-intro/",
                 },
               ],
             },
