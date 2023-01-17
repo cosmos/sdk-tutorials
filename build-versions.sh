c996b1d1df2f36fadb262449a6fc2e860fb79be4
Gioele Bonifetto <gioele@b9lab.com>
2023-01-17 16:33:32 +0100

    minor fix

--- build-versions.sh
+++ build-versions.sh
@@ -5,16 +5,17 @@ echo "\nBuild platform versions start\n"
 echo "\nBuild current version\n"
 npm ci && npm run build
 
-echo "\Move generated files to ~/output/ folder\n"
+echo "\nMove generated files to ~/output/ folder\n"
 mkdir -p ~/output && cp -r .vuepress/dist/* ~/output/
 
 echo "\nBuild other versions\n"
 versions=${DOCS_VERSIONS[@]}
 for branch in $versions; do \
-    echo "\nBuild docs version $branch\n" ; \
+    echo "\nSwitch to $branch version\n" ; \
     git clean -fdx && git reset --hard && git checkout $branch && git submodule update ; \
+    echo "\nBuild $branch version\n" ; \
     npm ci && VUEPRESS_BASE="/$branch/" npm run build --no-cache ; \
-    echo "\Move generated files to ~/output/$branch/ folder\n"
+    echo "\nMove generated files to ~/output/$branch/ folder\n"
     mkdir -p ~/output/$branch && cp -r .vuepress/dist/* ~/output/$branch/ ; \
 done
 
