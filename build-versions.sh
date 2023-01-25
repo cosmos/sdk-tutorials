516caea5e399b08642e6433c18b4b9dc9fb11b59
Ibo Sy <ibo@b9lab.com>
2023-01-25 12:09:35 +0100

    update build script, add versions text file

--- build-versions.sh
+++ build-versions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+VERSIONS_FILE="./versions.txt"
 
 echo -e "\nBuild platform versions start\n"
 
@@ -9,7 +10,11 @@ echo "Move generated files to ~/output/ folder"
 mkdir -p ~/output && cp -r .vuepress/dist/* ~/output/
 
 echo -e "\nBuild other versions\n"
+DOCS_VERSIONS=$(cat "$VERSIONS_FILE")
+export DOCS_VERSIONS=$DOCS_VERSIONS
+
 versions=${DOCS_VERSIONS[@]}
+
 for branch in $versions; do \
     echo "Switch to $branch version" ; \
     git clean -fdx && git reset --hard && git checkout $branch && git submodule update ; \
