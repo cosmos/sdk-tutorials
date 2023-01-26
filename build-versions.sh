fcdec68ced53574d08692438a1410d32ec131176
Ibo Sy <ibo@b9lab.com>
2023-01-26 14:28:33 +0100

    adjust temp build folder, update netlify settings

--- build-versions.sh
+++ build-versions.sh
@@ -8,8 +8,8 @@ echo -e "\nBuild platform versions start\n"
 echo "Build current version"
 npm ci && npm run build
 
-echo "Move generated files to ~/output/ folder"
-mkdir -p ~/output && cp -r .vuepress/dist/* ~/output/
+echo "Move generated files to /tmp/versions-build/ folder"
+mkdir -p /tmp/versions-build && cp -r .vuepress/dist/* /tmp/versions-build
 
 echo -e "\nBuild other versions\n"
 
@@ -20,8 +20,8 @@ for branch in $versions; do \
     git clean -fdx && git reset --hard && git checkout $branch && git submodule update ; \
     echo "Build $branch version" ; \
     npm ci && VUEPRESS_BASE="/$branch/" npm run build --no-cache ; \
-    echo "Move generated files to ~/output/$branch/ folder"
-    mkdir -p ~/output/$branch && cp -r .vuepress/dist/* ~/output/$branch/ ; \
+    echo "Move generated files to /tmp/versions-build/$branch/ folder"
+    mkdir -p /tmp/versions-build/$branch && cp -r .vuepress/dist/* /tmp/versions-build/$branch/ ; \
 done
 
 echo -e "\nBuild platform versions end\n"
