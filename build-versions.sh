6745d7eb2bfa18445c8763e84276486146f00b4e
Ibo Sy <ibo@b9lab.com>
2023-01-26 14:41:32 +0100

    adjust build script

--- build-versions.sh
+++ build-versions.sh
@@ -3,13 +3,17 @@
 VERSIONS_FILE="./versions.txt"
 export DOCS_VERSIONS=$(cat "$VERSIONS_FILE")
 
+if [ -z "${VERSIONS_BUILD_PATH}" ]; then
+    export VERSIONS_BUILD_PATH="~/output/"
+fi
+
 echo -e "\nBuild platform versions start\n"
 
 echo "Build current version"
 npm ci && npm run build
 
 echo "Move generated files to /tmp/versions-build/ folder"
-mkdir -p /tmp/versions-build && cp -r .vuepress/dist/* /tmp/versions-build
+mkdir -p $VERSIONS_BUILD_PATH && cp -r .vuepress/dist/* $VERSIONS_BUILD_PATH
 
 echo -e "\nBuild other versions\n"
 
@@ -21,7 +25,7 @@ for branch in $versions; do \
     echo "Build $branch version" ; \
     npm ci && VUEPRESS_BASE="/$branch/" npm run build --no-cache ; \
     echo "Move generated files to /tmp/versions-build/$branch/ folder"
-    mkdir -p /tmp/versions-build/$branch && cp -r .vuepress/dist/* /tmp/versions-build/$branch/ ; \
+    mkdir -p "${VERSIONS_BUILD_PATH}${branch}" && cp -r .vuepress/dist/* "${VERSIONS_BUILD_PATH}${branch}" ; \
 done
 
 echo -e "\nBuild platform versions end\n"
