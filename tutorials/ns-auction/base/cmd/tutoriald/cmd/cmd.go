939acd23be7dc0e1c46b68c05462145f93286667
samricotta <37125168+samricotta@users.noreply.github.com>
2024-01-18 13:17:15 +0100

    fix gitignore

---
+++ tutorials/ns-auction/base/cmd/tutoriald/cmd/cmd.go
@@ -0,0 +1,111 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/cosmos/sdk-tutorials/tutorials/ns-auction/x/ns-auction/testutils"
+
+	"cosmossdk.io/log"
+	dbm "github.com/cosmos/cosmos-db"
+	"github.com/cosmos/cosmos-sdk/codec"
+	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	"github.com/cosmos/cosmos-sdk/x/auth/tx"
+	txmodule "github.com/cosmos/cosmos-sdk/x/auth/tx/config"
+
+	"github.com/cosmos/sdk-tutorials/tutorials/ns-auction/base/app"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/config"
+	"github.com/cosmos/cosmos-sdk/server"
+
+	"github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/spf13/cobra"
+)
+
+func NewRootCmd() (*cobra.Command, testutils.EncodingConfig) {
+	tmpApp := app.NewTutorialApp(
+		log.NewNopLogger(),
+		dbm.NewMemDB(),
+		nil,
+		true,
+		map[int64]bool{},
+		"val",
+		simtestutil.NewAppOptionsWithFlagHome(tempDir()),
+	)
+
+	encodingConfig := testutils.EncodingConfig{
+		InterfaceRegistry: tmpApp.InterfaceRegistry(),
+		Marshaler:         tmpApp.AppCodec(),
+		TxConfig:          tmpApp.GetTxConfig(),
+		Amino:             tmpApp.LegacyAmino(),
+	}
+
+	initClientCtx := client.Context{}.
+		WithCodec(encodingConfig.Marshaler).
+		WithInterfaceRegistry(encodingConfig.InterfaceRegistry).
+		WithTxConfig(encodingConfig.TxConfig).
+		WithLegacyAmino(encodingConfig.Amino).
+		WithInput(os.Stdin).
+		WithAccountRetriever(types.AccountRetriever{}).
+		WithHomeDir(app.DefaultNodeHome).
+		WithViper("")
+
+	rootCmd := &cobra.Command{
+		Use:   app.AppName + "d",
+		Short: "Cosmos App",
+		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
+			cmd.SetOut(cmd.OutOrStdout())
+			cmd.SetErr(cmd.ErrOrStderr())
+
+			initClientCtx = initClientCtx.WithCmdContext(cmd.Context())
+			initClientCtx, err := client.ReadPersistentCommandFlags(initClientCtx, cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			initClientCtx, err = config.ReadFromClientConfig(initClientCtx)
+			if err != nil {
+				return err
+			}
+
+			// This needs to go after ReadFromClientConfig, as that function
+			// sets the RPC client needed for SIGN_MODE_TEXTUAL.
+			enabledSignModes := append(tx.DefaultSignModes, signing.SignMode_SIGN_MODE_TEXTUAL)
+			txConfigOpts := tx.ConfigOptions{
+				EnabledSignModes:           enabledSignModes,
+				TextualCoinMetadataQueryFn: txmodule.NewGRPCCoinMetadataQueryFn(initClientCtx),
+			}
+			txConfigWithTextual, err := tx.NewTxConfigWithOptions(
+				codec.NewProtoCodec(encodingConfig.InterfaceRegistry),
+				txConfigOpts,
+			)
+			if err != nil {
+				return err
+			}
+			initClientCtx = initClientCtx.WithTxConfig(txConfigWithTextual)
+
+			if err = client.SetCmdClientContextHandler(initClientCtx, cmd); err != nil {
+				return err
+			}
+
+			customAppTemplate, customAppConfig := initAppConfig()
+			customTMConfig := initTendermintConfig()
+
+			return server.InterceptConfigsPreRunHandler(cmd, customAppTemplate, customAppConfig, customTMConfig)
+		},
+	}
+
+	initRootCmd(rootCmd, encodingConfig, tmpApp.BasicManager, app.DefaultNodeHome)
+
+	// // // add keyring to autocli opts
+	// autoCliOpts := tmpApp.AutoCliOpts()
+	// initClientCtx, _ = config.ReadFromClientConfig(initClientCtx)
+	// // autoCliOpts.Keyring = initClientCtx.Keyring
+
+	// if err := autoCliOpts.EnhanceRootCommand(rootCmd); err != nil {
+	// 	panic(err)
+	// }
+
+	return rootCmd, encodingConfig
+}
