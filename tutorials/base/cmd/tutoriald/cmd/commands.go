8c4b14a040722c2d9e60f63d9d9d108f6d04469d
samricotta <37125168+samricotta@users.noreply.github.com>
2024-01-18 17:09:13 +0100

    directory alteration

--- tutorials/base/cmd/tutoriald/cmd/commands.go
+++ tutorials/base/cmd/tutoriald/cmd/commands.go
@@ -3,16 +3,14 @@ package cmd
 import (
 	"errors"
 	"io"
-	"os"
 
-	"github.com/cosmos/sdk-tutorials/tutorials/ns-auction/testutils"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	dbm "github.com/cosmos/cosmos-db"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"cosmossdk.io/log"
 	confixcmd "cosmossdk.io/tools/confix/cmd"
-	tmcfg "github.com/cometbft/cometbft/config"
-	dbm "github.com/cosmos/cosmos-db"
+
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/debug"
 	"github.com/cosmos/cosmos-sdk/client/flags"
@@ -21,81 +19,37 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/rpc"
 	"github.com/cosmos/cosmos-sdk/client/snapshot"
 	"github.com/cosmos/cosmos-sdk/server"
-	serverconfig "github.com/cosmos/cosmos-sdk/server/config"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
-	"github.com/cosmos/cosmos-sdk/x/crisis"
 	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
-	"github.com/cosmos/sdk-tutorials/tutorials/base/app"
-	ns "github.com/cosmos/sdk-tutorials/tutorials/ns-auction"
-	nsmod "github.com/cosmos/sdk-tutorials/tutorials/ns-auction/module"
-	"github.com/spf13/cast"
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+
+	"github.com/cosmos/sdk-tutorials/app"
 )
 
-func initTendermintConfig() *tmcfg.Config {
-	cfg := tmcfg.DefaultConfig()
-
-	return cfg
-}
-
-func initAppConfig() (string, interface{}) {
-	type CustomAppConfig struct {
-		serverconfig.Config
-	}
-
-	srvCfg := serverconfig.DefaultConfig()
-	srvCfg.StateSync.SnapshotInterval = 1000
-	srvCfg.StateSync.SnapshotKeepRecent = 10
-
-	customAppConfig := CustomAppConfig{
-		Config: *srvCfg,
-	}
-
-	defaultAppTemplate := serverconfig.DefaultConfigTemplate
-
-	return defaultAppTemplate, customAppConfig
-}
-
-func initRootCmd(rootCmd *cobra.Command, encodingConfig testutils.EncodingConfig, basicManager module.BasicManager, defaultNodeHome string) {
+func initRootCmd(rootCmd *cobra.Command, txConfig client.TxConfig, basicManager module.BasicManager) {
 	cfg := sdk.GetConfig()
 	cfg.Seal()
 
 	rootCmd.AddCommand(
-		genutilcli.InitCmd(basicManager, defaultNodeHome),
+		genutilcli.InitCmd(basicManager, app.DefaultNodeHome),
 		debug.Cmd(),
 		confixcmd.ConfigCommand(),
-		pruning.Cmd(newApp, defaultNodeHome),
+		pruning.Cmd(newApp, app.DefaultNodeHome),
 		snapshot.Cmd(newApp),
 	)
 
-	server.AddCommands(rootCmd, app.DefaultNodeHome, newApp, appExport, addModuleInitFlags)
+	server.AddCommands(rootCmd, app.DefaultNodeHome, newApp, appExport, func(startCmd *cobra.Command) {})
 
+	// add keybase, auxiliary RPC, query, genesis, and tx child commands
 	rootCmd.AddCommand(
 		server.StatusCommand(),
-		genesisCommand(encodingConfig, app.DefaultNodeHome, basicManager),
+		genutilcli.Commands(txConfig, basicManager, app.DefaultNodeHome),
 		queryCommand(),
 		txCommand(),
 		keys.Commands(),
 	)
-
-}
-
-func addModuleInitFlags(startCmd *cobra.Command) {
-	crisis.AddModuleInitFlags(startCmd)
-	startCmd.Flags().String(ns.FlagValKey, "", "Name of Validator Key to Sign Txs")
-	startCmd.Flags().String(ns.FlagRunProvider, "false", "Run the transaction provider logic")
-}
-
-func genesisCommand(encodingConfig testutils.EncodingConfig, defaultNodeHome string, basicManager module.BasicManager, cmds ...*cobra.Command) *cobra.Command {
-	cmd := genutilcli.Commands(encodingConfig.TxConfig, basicManager, defaultNodeHome)
-
-	for _, subCmd := range cmds {
-		cmd.AddCommand(subCmd)
-	}
-	return cmd
 }
 
 func queryCommand() *cobra.Command {
@@ -103,19 +57,18 @@ func queryCommand() *cobra.Command {
 		Use:                        "query",
 		Aliases:                    []string{"q"},
 		Short:                      "Querying subcommands",
-		DisableFlagParsing:         true,
+		DisableFlagParsing:         false,
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
 
 	cmd.AddCommand(
-		rpc.QueryEventForTxCmd(),
+		rpc.ValidatorCommand(),
 		server.QueryBlockCmd(),
 		authcmd.QueryTxsByEventsCmd(),
 		server.QueryBlocksCmd(),
 		authcmd.QueryTxCmd(),
 		server.QueryBlockResultsCmd(),
-		nsmod.QueryWhoisCmd(),
 	)
 
 	return cmd
@@ -125,7 +78,7 @@ func txCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:                        "tx",
 		Short:                      "Transactions subcommands",
-		DisableFlagParsing:         true,
+		DisableFlagParsing:         false,
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
@@ -140,42 +93,23 @@ func txCommand() *cobra.Command {
 		authcmd.GetEncodeCommand(),
 		authcmd.GetDecodeCommand(),
 		authcmd.GetSimulateCmd(),
-		nsmod.ReserveCmd(),
 	)
 
 	return cmd
 }
 
-func newApp(
-	logger log.Logger,
-	db dbm.DB,
-	traceStore io.Writer,
-	appOpts servertypes.AppOptions,
-) servertypes.Application {
+// newApp is an appCreator
+func newApp(logger log.Logger, db dbm.DB, traceStore io.Writer, appOpts servertypes.AppOptions) servertypes.Application {
 	baseappOptions := server.DefaultBaseappOptions(appOpts)
-
-	skipUpgradeHeights := make(map[int64]bool)
-	for _, h := range cast.ToIntSlice(appOpts.Get(server.FlagUnsafeSkipUpgrades)) {
-		skipUpgradeHeights[int64(h)] = true
+	app, err := app.NewTutorialApp(logger, db, traceStore, true, appOpts, baseappOptions...)
+	if err != nil {
+		panic(err)
 	}
 
-	valKey, ok := appOpts.Get(ns.FlagValKey).(string)
-	if !ok {
-		valKey = "val"
-	}
-
-	return app.NewTutorialApp(
-		logger,
-		db,
-		traceStore,
-		true,
-		skipUpgradeHeights,
-		valKey,
-		appOpts,
-		baseappOptions...,
-	)
+	return app
 }
 
+// appExport creates a new app (optionally at a given height) and exports state.
 func appExport(
 	logger log.Logger,
 	db dbm.DB,
@@ -186,13 +120,13 @@ func appExport(
 	appOpts servertypes.AppOptions,
 	modulesToExport []string,
 ) (servertypes.ExportedApp, error) {
-	var exportApp *app.TutorialApp
+	var (
+		tutorialApp *app.TutorialApp
+		err         error
+	)
 
-	valKey, ok := appOpts.Get(ns.FlagValKey).(string)
-	if !ok {
-		return servertypes.ExportedApp{}, errors.New("validator key not set")
-	}
-
+	// this check is necessary as we use the flag in x/upgrade.
+	// we can exit more gracefully by checking the flag here.
 	homePath, ok := appOpts.Get(flags.FlagHome).(string)
 	if !ok || homePath == "" {
 		return servertypes.ExportedApp{}, errors.New("application home not set")
@@ -202,39 +136,26 @@ func appExport(
 	if !ok {
 		return servertypes.ExportedApp{}, errors.New("appOpts is not viper.Viper")
 	}
+
+	// overwrite the FlagInvCheckPeriod
 	viperAppOpts.Set(server.FlagInvCheckPeriod, 1)
 	appOpts = viperAppOpts
 
-	var loadLatest bool
-	if height == -1 {
-		loadLatest = true
-	}
-
-	exportApp = app.NewTutorialApp(
-		logger,
-		db,
-		traceStore,
-		loadLatest,
-		map[int64]bool{},
-		valKey,
-		appOpts,
-	)
-
 	if height != -1 {
-		if err := exportApp.LoadHeight(height); err != nil {
+		tutorialApp, err = app.NewTutorialApp(logger, db, traceStore, false, appOpts)
+		if err != nil {
+			return servertypes.ExportedApp{}, err
+		}
+
+		if err := tutorialApp.LoadHeight(height); err != nil {
+			return servertypes.ExportedApp{}, err
+		}
+	} else {
+		tutorialApp, err = app.NewTutorialApp(logger, db, traceStore, true, appOpts)
+		if err != nil {
 			return servertypes.ExportedApp{}, err
 		}
 	}
 
-	return exportApp.ExportAppStateAndValidators(forZeroHeight, jailAllowedAddrs, modulesToExport)
-}
-
-var tempDir = func() string {
-	dir, err := os.MkdirTemp("", "tutorial")
-	if err != nil {
-		dir = app.DefaultNodeHome
-	}
-	defer os.RemoveAll(dir)
-
-	return dir
+	return tutorialApp.ExportAppStateAndValidators(forZeroHeight, jailAllowedAddrs, modulesToExport)
 }
--- x/oracle/errors.go
+++
@@ -1,8 +0,0 @@
-package oracle
-
-import "cosmossdk.io/errors"
-
-var (
-	// ErrDuplicateAddress error if there is a duplicate address
-	ErrDuplicateAddress = errors.Register(ModuleName, 2, "duplicate address")
-)
