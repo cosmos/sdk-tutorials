ee8902d365c186200ca343c4598f08d6c16d7e02
samricotta <37125168+samricotta@users.noreply.github.com>
2024-01-19 10:36:40 +0100

    feat: Initialise Tutorial Example Project (#1498)
    * setup and working
    
    * remove readme
    
    * replace goleveldb
    
    * change directories
    
    ---------
    
    Co-authored-by: Facundo <facundomedica@gmail.com>

---
+++ tutorials/base/scripts/init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+rm -r ~/.tutoriald || true
+tutorialD_BIN=$(which tutoriald)
+# configure tutoriald
+$tutorialD_BIN config set client chain-id demo
+$tutorialD_BIN config set client keyring-backend test
+$tutorialD_BIN keys add alice
+$tutorialD_BIN keys add bob
+$tutorialD_BIN init test --chain-id demo --default-denom tutorial
+# update genesis
+$tutorialD_BIN genesis add-genesis-account alice 10000000tutorial --keyring-backend test
+$tutorialD_BIN genesis add-genesis-account bob 1000tutorial --keyring-backend test
+# create default validator
+$tutorialD_BIN genesis gentx alice 1000000tutorial --chain-id demo
+$tutorialD_BIN genesis collect-gentxs
