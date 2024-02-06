eb728e625162ea45361321765b110ff144b22c2e
samricotta <37125168+samricotta@users.noreply.github.com>
2024-02-06 11:41:31 +0100

    update ve

--- tutorials/base/cmd/exampled/cmd/root.go
+++ tutorials/base/cmd/exampled/cmd/root.go
@@ -101,7 +101,7 @@ func NewRootCmd() *cobra.Command {
 
 			// overwrite the minimum gas price from the app configuration
 			srvCfg := serverconfig.DefaultConfig()
-			srvCfg.MinGasPrices = "0tutorial"
+			srvCfg.MinGasPrices = "0example"
 
 			// overwrite the block timeout
 			cmtCfg := cmtcfg.DefaultConfig()
--- tutorials/oracle/base/scripts/init.sh
+++ tutorials/oracle/base/scripts/init.sh
@@ -14,4 +14,5 @@ $exampled_BIN genesis add-genesis-account bob 1000example --keyring-backend test
 # create default validator
 $exampled_BIN genesis gentx alice 1000000example --chain-id demo
 sed -i.bak'' 's/minimum-gas-prices = ""/minimum-gas-prices = "0.025uatom"/' $HOME/.exampled/config/app.toml
+jq '.consensus.params.abci.vote_extensions_enable_height = "1"' ~/.exampled/config/genesis.json > output.json && mv output.json ~/.exampled/config/genesis.json
 $exampled_BIN genesis collect-gentxs
